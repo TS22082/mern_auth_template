a151c9779f0db9acc9cac2f247a0f970ba9153ba
TS22082 <ts22082@gmail.com>
2020-03-09 16:24:12 -0700

    add mongodb connection

--- server/server.js
+++ server/server.js
@@ -2,20 +2,39 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
 const mongoose = require("mongoose");
 
-require("dotenv").config();
-const PORT = process.env.PORT || 5000;
+const MONGO_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/mern_auth_template";
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  })
+  .then(() => console.log("connected to db instance"))
+  .catch(err => console.log(err));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+if (process.env.NODE_ENV === "development") {
+  app.use(cors({ origin: `http://localhost:3000` }));
+}
 
 // middleware
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(morgan("dev"));
+
+// routes
 const authRoutes = require("./routes/auth-routes");
 app.use("/auth", authRoutes);
 
